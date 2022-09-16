809727ffa4b378c0fe8d546507f091f0c86cc5e6
Divy0603 <100995948+Divy0603@users.noreply.github.com>
2022-09-16 19:47:25 +0530

    Create medical2.java

---
+++ medical2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.lang.String;
+public class medical2 {
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of classes held");
+        float held = sc.nextInt();
+        System.out.println("enter the number of classes attended");
+        float attended = sc.nextInt();
+        System.out.println("do you have a medical cause");
+        String med = sc.next();
+        float percentage =  (attended/held) * 100;
+        
+        System.out.println(percentage);
+        if (med .equals("yes")){
+            System.out.println("you are allowed");
+
+        }
+        else if(med.equals("no")){
+            if (percentage > 75){
+                System.out.println("you arre allowed to sit in the exam");
+    
+            }
+            else{
+                System.out.println("you are not allowed to enter the exam");
+    
+            }
+        }
+         
+    }
+}
+
