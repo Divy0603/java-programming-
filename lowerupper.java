7311889a33e5e673562fb8a81518b5e2cae87914
Divy0603 <100995948+Divy0603@users.noreply.github.com>
2022-09-16 19:45:54 +0530

    Create lowerupper.java

---
+++ lowerupper.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class lowerupper {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String sir= sc.next();
+        if (sir.equals(sir.toUpperCase())){
+            System.out.println("upper");
+            
+        }
+        else{
+            System.out.println("lower");
+        }
+        }
+
+    }
+
